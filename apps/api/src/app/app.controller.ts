cd68e642e5d44c648986055bc4236cc8a138a8da
Joar K. Mauseth <jkm99412@gmail.com>
2021-05-15 12:40:41 +0200

    Added swagger for api documentation

--- apps/api/src/app/app.controller.ts
+++ apps/api/src/app/app.controller.ts
@@ -1,9 +1,11 @@
 import { Controller, Get } from '@nestjs/common';
+import { ApiTags } from '@nestjs/swagger';
 
 import { Message } from '@nx-workspace/api-interfaces';
 
 import { AppService } from './app.service';
 
+@ApiTags('app')
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -5,6 +5,7 @@
 
 import { Logger } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 import { AppModule } from './app/app.module';
 
@@ -13,6 +14,17 @@ async function bootstrap() {
   const globalPrefix = 'api';
   app.setGlobalPrefix(globalPrefix);
   const port = process.env.PORT || 6333;
+
+  //#region Swagger
+  const config = new DocumentBuilder()
+  .setTitle('Api documentation')
+  .setDescription('API description')
+  .setVersion('1.0')
+  .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+  //#endregion
+
   await app.listen(port, () => {
     Logger.log('Listening at http://localhost:' + port + '/' + globalPrefix);
   });
